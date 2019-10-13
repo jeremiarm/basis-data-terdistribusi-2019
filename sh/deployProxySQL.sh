cbb816cc09a108f9f78df8c26d1a77fcf9915f05
Jeremia Ronaldo Manurung <jeremiarm5@gmail.com>
2019-10-13 22:22:56 +0700

    instalasi awal dan memasukin project Meeting Room

--- sh/deployProxySQL.sh
+++ sh/deployProxySQL.sh
@@ -22,4 +22,6 @@ sudo ufw allow 33061
 sudo ufw allow 3306
 
 sudo systemctl start proxysql
-mysql -u admin -padmin -h 127.0.0.1 -P 6032 < /vagrant/proxysql.sql
+#mysql -u admin -p -h 127.0.0.1 -P 6032 < /vagrant/sql/proxysql.sql
+#password : admin 
+#password : password
--- sql/create_proxysql_user.sql
+++ sql/create_proxysql_user.sql
@@ -2,6 +2,6 @@ CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitorpassword';
 GRANT SELECT on sys.* to 'monitor'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'playgrounduser'@'%' IDENTIFIED BY 'playgroundpassword';
-GRANT ALL PRIVILEGES on playground.* to 'playgrounduser'@'%';
+CREATE USER 'mradmin'@'%' IDENTIFIED BY 'mradmin';
+GRANT ALL PRIVILEGES on meetingroom.* to 'mradmin'@'%';
 FLUSH PRIVILEGES;
